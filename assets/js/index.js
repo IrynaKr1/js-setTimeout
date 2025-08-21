3aefdd6ab7ebbebe5790933b323ad3d1c26428b8
IrynaKr1 <krutsiryna@gmail.com>
2025-08-21 19:56:09 +0200

    Correct printNumbers function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,20 @@ const button = document.querySelector('button');
 const waitInfo = document.querySelector('.waitInfo');
 const pageLink = document.querySelector('.pageLink');
 
-
 function printNumbers(from, to, interval) {
   let current = from;
+  timer.textContent = from;
 
   if (current < to) {
-    const timer = setInterval(() => {
+    const intervals = setInterval(() => {
       current++;
-      console.log(current);
+      timer.textContent = current;
+
       if (current >= to) {
-        clearInterval(timer);
+        clearInterval(intervals);
       }
     }, interval);
   }
 }
 
-console.log(printNumbers(2, 7, 1000));
+printNumbers(2,15,1000);
